ecc10b1a59c7794ce95bfbb171fdf0ccaf1028a9
Eliezer <frete7132@gmail.com>
2023-02-16 21:12:27 -0300

    insertion of the fxml document with the program

---
+++ src/gui/ViewController.java
@@ -0,0 +1,18 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ViewController {
+	
+	@FXML
+	private Button btTeste;
+	
+	@FXML
+	public void onBtTestAction() {
+		System.out.println("opa");
+	}
+	
+	
+
+}

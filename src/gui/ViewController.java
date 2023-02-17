28e647ce8f8d575d792b36d838e4765e666c2657
Eliezer <frete7132@gmail.com>
2023-02-16 21:49:31 -0300

    character limitations

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,17 +1,52 @@
 package gui;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
-public class ViewController {
+import gui.util.Alerts;
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ViewController implements Initializable {
 	
 	@FXML
-	private Button btTeste;
-	
+	private TextField txtNumero1;
 	@FXML
-	public void onBtTestAction() {
-		System.out.println("opa");
+	private TextField txtNumero2;
+	@FXML
+	private Label labelResult;
+	@FXML
+	private Button btsun;
+	
+	@FXML 
+	public void onBtAction() {
+		try {
+		Locale.setDefault(Locale.US);
+		double number1 = Double.parseDouble(txtNumero1.getText());
+		double number2 = Double.parseDouble(txtNumero2.getText());
+		double sun = number1 + number2;
+		labelResult.setText(String.format("%.2f",sun));
+		}catch (NumberFormatException e) {
+			Alerts.showAlert("Error", "Parse error", e.getMessage(), AlertType.ERROR);
+			// TODO: handle exception
+		}
 	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		Constraints.setTextFieldDouble(txtNumero1);
+		Constraints.setTextFieldDouble(txtNumero2);
+		Constraints.setTextFieldMaxLength(txtNumero1, 2);
+		Constraints.setTextFieldMaxLength(txtNumero2, 2);
+		
+	}
+
 	
 	
 
